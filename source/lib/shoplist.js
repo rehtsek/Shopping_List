b9fe14c85627ba050120889f42f8b297c6f7dd5d
DESKTOP-CCTALOB\Yinka <esthery19@gmail.com>
2017-01-06 01:02:51 +0000

    add and delete button updated

--- source/lib/shoplist.js
+++ source/lib/shoplist.js
@@ -1,31 +1,24 @@
 //$(document).ready(function(){
-  //alert('page is ready');
-//var data = {};
-// var myList = displayShoppingList(getList());
-
-//alert(myList);
-//$('#display-shopping-list').html(myList);
-// myList = '<ul data-role="listview">'+
-//     '<li><a href="#">Acura</a></li>'+
-//     '<li><a href="#">Audi</a></li>'+
-//     '<li><a href="#">BMW</a></li>'+
-//     '<li><a href="#">Cadillac</a></li>'+
-//     '<li><a href="#">Ferrari</a></li>'+
-// '</ul>';
-
-
-  $(document).on('pagebeforechange', '#index', function(){
-
-    alert( "testing!" );
+  
+ //alert('hello');
+ $(document).on("pagebeforecreate","#index",function(){
+  //alert("pagebeforeshow event fired - pagetwo is about to be shown");
+  //$('#display-shopping-list').html("<li>Hello</li><li>World</li>");
+  showHomepage();
+  
 });
-  $("#index").on( "pagebeforehide", function( event ) {
+ /*$(document).on("pagebeforeshow","#index",function(){
+  alert("pagebeforeshow event fired - pagetwo is about to be shown");
+  //$('#display-shopping-list').html("<li>Hello</li><li>World</li>");
+  //showHomepage();
+  
+});*/
 
-    alert("This leaving index page");
-  } );
-  $( ".selector" ).on( "pagecontainerbeforechange", function() {
-    alert('change');
-  } );
-  var getList = getList();
+ //showHomepage();
+  //alert("I am adding item to the list");
+  
+
+  /*var getList = getList();
 
   var myShoppingList = displayShoppingList(getList);
    console.log(myShoppingList);
@@ -33,34 +26,138 @@
   var listItems = myShoppingList.shopping_lists_items;
   console.log(listItems);
     $(document).on("pagebeforecreate",function(){
-  //alert("pagecreate event fired - the page has been created, but enhancement is not complete");
-    $('#display-shopping-list').html(myList);
+     $('#display-shopping-list').html(myList);
+    if(listItems.length > 0){
     for(var j = 0; j < listItems.length; j++){
 
       $( "body" ).append(listItems[j]);
-
+    
     }
-});
+    }
+  });*/
+function showHomepage(){
+var shoppingList = JSON.parse(localStorage.getItem("ekShoppingList"));
+var shoppingListObject = {};
+  var htmlShoppingItemsData = [];
+  var htmlList = "";
+  var htmlShoppingItem = ""; 
+  $.each(shoppingList, function( key, value ){
+    console.log(value);
+  htmlList += '<li><a href="#'+key+'">'+key+'</a></li>';
+  var shoppingItem = '<div data-role="page" id="'+key+'">'+
+                     '<header data-role="header" data-add-back-btn="true">'+
+                      '<h1>'+key+' : Shopping Items.</h1>'+
+                     '</header>'+
+                     '<div data-role="content">'+
+                     '<h3>'+key+' : Shopping Items.</h3>'+
+                     '<div class="ui-field-contain">'+
+                     '<fieldset data-role="controlgroup" data-type="horizontal">'+
+                  //         '<legend></legend>'+
+                          '<button class="ui-shadow ui-btn ui-corner-all ui-icon-home ui-btn-icon-right" value="'+key+'">Edit Item</button>'+
+                          '<button class="ui-shadow ui-btn ui-corner-all ui-icon-home ui-btn-icon-right" value="'+key+'">Delete</button>'+
+                         
+                          //<a href="#" class="ui-shadow ui-btn ui-corner-all ui-icon-grid ui-btn-icon-right">Three</a>
+                      '</fieldset>'+
+                      '</div>'+
+                      '<form name="'+key+'" id="'+key+'">'+
+                       '<div class="ui-grid-b ui-responsive">'+
+                       '<div class="ui-block-a" style="margin-right: 5px;">'+
+                            '<input type="text" name="new-shopping-list" class="new-list-item" id="'+key+'-new-list-item" placeholder="add new item to list" value="">'+
+                       '</div>'+
+                       '<div class="ui-block-b">'+
+                            '<input type="text" name="quantity" class="quantity" id="'+key+'-quantity" placeholder="1" value="">'+
+                       '</div>'+
+                         '<div class="ui-block-c">'+
+                            '<a href="#" data-'+key+'-list="'+key+'" class="ui-btn ui-corner-all ui-shadow add-list-item" id="'+key+'-add-list-item">Add Item</a>'+
+                         '</div>'+
+                         '</div>'+
+                      '</form>'+
+                        '<ul data-role="listview" class="list-items" id="list-items" data-theme="b">';
+  
+  for(var i = 0; i<value.length; i++){
+          shoppingItem += '<li><a href="#'+value[i].id+'">'+value[i].item+': '+value[i].quantity+'</a></li>';
+  }
+          shoppingItem += '</ul></div></div>';
 
-$( "#index" ).pagecontainer({
-
-  //beforetransition: function( event, ui ) {}
-  beforetransition: function(){
-    alert('good');
-    alert('transitioning');
-    var getList = getList();
-
-  var myShoppingList = displayShoppingList(getList);
-   console.log(myShoppingList);
-  myList = myShoppingList.shopping_lists;
-  var listItems = myShoppingList.shopping_lists_items;
-    $('#display-shopping-list').html(myList);
+          htmlShoppingItemsData.push(shoppingItem);
+ });
+ var shoppingListObject = {
+  'shopping_lists' : htmlList,
+  'shopping_lists_items' : htmlShoppingItemsData
+ };
+ myList = shoppingListObject.shopping_lists;
+  var listItems = shoppingListObject.shopping_lists_items;
+  console.log(listItems);
+    
+     $('#display-shopping-list').html(myList);
+    if(listItems.length > 0){
     for(var j = 0; j < listItems.length; j++){
 
       $( "body" ).append(listItems[j]);
-
+    
     }
-}
+    }
+
+}  
+  /*$.mobile.document.on('pagebeforeshow', function() {
+    alert($.mobile.activePage.attr('id'));
+  });*/
+  $.mobile.document.on( "click", "#add-shopping-item", function() {  
+  //console.log('good');
+  //alert("I am adding item to the list");
+  //return false;
+  
+  var newItem = $('#new-shopping-list').val();
+  console.log(creatNewList(newItem));
+  //showHomepage();
+   //$("#index").enhanceWithin();
+  var htmlList = '<li><a href="#'+newItem+'">'+newItem+'</a></li>';
+  var shoppingItem = '<div data-role="page" id="'+newItem+'">'+
+                     '<header data-role="header" data-add-back-btn="true">'+
+                      '<h1>'+newItem+' : Shopping Items.</h1>'+
+                     '</header>'+
+                     '<div data-role="content">'+
+                     '<ul data-role="listview" data-theme="b">'+
+                     '</ul>'+
+                     '</div>'+
+                     '</div>';
+   
+   console.log(htmlList);
+    $( htmlList ).appendTo("#display-shopping-list");
+    $( "body" ).append(shoppingItem);
+    $("#display-shopping-list").listview("refresh");
+    
+});                                                                                                                                                                                                                                                                                                                                                                                    ;
+//});  
+$.mobile.document.on( "click", ".add-list-item", function() { 
+  //$( "#target" ).submit(function( event ) {
+    var pageId = $.mobile.activePage.attr('id');
+//alert('sounds good');
+//return false; 
+  var newItem = $("#"+pageId+"-new-list-item").val();
+  var quantity = $('#'+pageId+'-quantity').val();
+  var shoppingList = $('#'+pageId+'-add-list-item').data(pageId+"List");
+  console.log(newItem, quantity, shoppingList);
+  return false;
+  
+  console.log(addItem(newItem, quantity, shoppingList));
+  //return false;
+  var htmlList = '<li><a href="#'+newItem+'">'+newItem+'</a></li>';
+  /*var shoppingItem = '<div data-role="page" id="'+newItem+'">'+
+                     '<header data-role="header" data-add-back-btn="true">'+
+                      '<h1>'+newItem+' : Shopping Items.</h1>'+
+                     '</header>'+
+                     '<div data-role="content">'+
+                     '<ul data-role="listview" data-theme="b">'+
+                     '</ul>'+
+                     '</div>'+
+                     '</div>';*/
+   
+   console.log(htmlList);
+    $( htmlList ).appendTo("#list-items");
+    //$( "body" ).append(shoppingItem);
+    $("#list-items").listview("refresh");
+    
 });
 
 // $('#display-shopping-list').html(myList);
@@ -85,17 +182,23 @@ if(existingEntries == null){ existingEntries = {} };
   //existingEntries[newItem] = newShoppingList;
  $.extend(existingEntries, newShoppingList );
 
-localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
+  localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
 //console.log(JSON.stringify(existingEntries));
-return JSON.stringify(existingEntries)
+  //showHomepage();
+  //$("#display-shopping-list").listview();
+  return JSON.stringify(existingEntries)
 }
 
-//adding item to a shopping list
+//adding item to a shopping list 
 //require: listName
-function addItem(listName){
+function addItem(listName, item, quantity){
+  var myShoppingLists = "ekShoppingList";
 	var existingEntries = JSON.parse(localStorage.getItem(myShoppingLists));
-	var entryItem = $("#item").val();
-    var entryQty = $("#quantity").val();
+	/*var entryItem = $("#item").val();
+    var entryQty = $("#quantity").val();*/
+    console.log(typeof existingEntries);
+    console.log(existingEntries.Alvihage);
+    //return false;
    var len = existingEntries[listName].length-1;
    if(len == -1){i=1}else{
     var i = existingEntries[listName][len];
@@ -103,7 +206,7 @@ function addItem(listName){
 	i = i+1;}
 	console.log(i);
   existingEntries[listName].push(
-    {id: i, item: entryItem, quantity: entryQty, status: "new"}
+    {id: i, item: item, quantity: quantity, status: "new"}
 );
 
 localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
@@ -127,11 +230,11 @@ $.each(keys,function(i,obj) {
 }
 function displayShoppingList(shoppingList){
 	//var existingEntries = JSON.parse(localStorage.getItem(myShoppingLists));
-
+  
   var shoppingListObject = {};
   var htmlShoppingItemsData = [];
   var htmlList = "";
-  var htmlShoppingItem = "";
+  var htmlShoppingItem = ""; 
   $.each(shoppingList, function( key, value ){
     console.log(value);
   htmlList += '<li><a href="#'+key+'">'+key+'</a></li>';
@@ -141,7 +244,7 @@ function displayShoppingList(shoppingList){
                      '</header>'+
                      '<div data-role="content">'+
                      '<ul data-role="listview" data-theme="b">';
-
+  
   for(var i = 0; i<value.length; i++){
           shoppingItem += '<li><a href="#'+value[i].id+'">'+value[i].item+': '+value[i].quantity+'</a></li>';
   }
@@ -153,7 +256,7 @@ function displayShoppingList(shoppingList){
   'shopping_lists' : htmlList,
   'shopping_lists_items' : htmlShoppingItemsData
  };
-
+ 
  return shoppingListObject;
  //return htmlList;
 
@@ -167,7 +270,7 @@ $( ".new-list" ).hide();
 $( "#shoplists" ).hide();
 $('<label for="male">'+listName+'</label>').appendTo("#itemLists");
 
-c.forEach(function(obj) {
+c.forEach(function(obj) { 
   $('<li>',{text:obj.item + ': '+obj.quantity }).appendTo("#itemLists");
   $('<input type="button" class="btn btn-primary" value="Del" onclick="deleteItem(\'' + listName + '\',\'id\',\'' + obj.id + '\');" />').appendTo("#itemLists");
   //console.log(obj.item);
@@ -181,21 +284,21 @@ $('<input type="button" class="btn btn-primary" value="Add" onclick="addItem(\''
 function updateStatus(listName, property, value){
 	 var existingEntries = JSON.parse(localStorage.getItem(myShoppingLists));
 	 $.each(existingEntries[listName], function(index, result) {
-
+        	
             if (result[property] == value) {
             	console.log(index);
 				existingEntries[listName][index].status = "bought";
                 return false;
             }
 
-        });
+        });   
 
     localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
 }
-
+	
    function deleteItem1(listName, index) {
    var existingEntries = JSON.parse(localStorage.getItem(myShoppingLists));
-   existingEntries[listName].splice(index,1);
+   existingEntries[listName].splice(index,1);  
 
     localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
 	console.log(JSON.stringify(existingEntries));
@@ -205,14 +308,14 @@ function updateStatus(listName, property, value){
    function deleteItem(listName, property, value) {
    var existingEntries = JSON.parse(localStorage.getItem(myShoppingLists));
         $.each(existingEntries[listName], function(index, result) {
-
+        	
             if (result[property] == value) {
             	console.log(index);
                 existingEntries[listName].splice(index,1);
                 return false;
             }
 
-        });
+        });   
 
     localStorage.setItem(myShoppingLists, JSON.stringify(existingEntries));
 }
